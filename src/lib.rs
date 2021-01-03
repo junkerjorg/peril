f6f263840746248109164d18d78e613347e403d6
Junker Jorg <junkerjorg21@protonmail.ch>
2021-01-03 06:11:27 -0500

    adding interface to check if the HazardPointer changed while scoped

--- src/lib.rs
+++ src/lib.rs
@@ -556,6 +556,29 @@ impl<'registry, 'hazard, T: Send + 'registry> HazardScope<'registry, 'hazard, T>
             Some(reference)
         }
     }
+
+    /// checks if the underlying atomic value already changed
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use peril::{HazardRegistry, HazardValue, HazardRecord, HazardPointer, Ordering};
+    ///
+    /// let registry = HazardRegistry::default();
+    /// let hp = HazardPointer::new(HazardValue::boxed(0), &registry);
+    /// let mut record = HazardRecord::default();
+    /// loop {
+    ///     let scope = hp.protect(&mut record);
+    ///     assert!(!scope.changed(Ordering::Relaxed));
+    ///     if scope.compare_exchange(HazardValue::boxed(1), Ordering::Relaxed, Ordering::Relaxed).is_ok()
+    ///     {
+    ///         break;
+    ///     }
+    /// }
+    /// ```
+    pub fn changed(&self, order: Ordering) -> bool {
+        self.current != self.ptr.atomic.load(order)
+    }
 }
 
 /// is the an atomic pointer that is safe to read when it is protected
